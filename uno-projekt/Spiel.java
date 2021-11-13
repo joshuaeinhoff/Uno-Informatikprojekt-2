f37492de20ab79ec4c42edffbd0550cfd8d430e4
Teresa Rodriguez Fuentes <tsrodf@gmail.com>
2021-11-13 19:02:48 +0100

    Spiel updated

--- uno-projekt/Spiel.java
+++ uno-projekt/Spiel.java
@@ -1,5 +1,5 @@
 /**
- *  Klasse Spiel -> besser benennen 
+ *  Klasse Spiel 
  */
 class Spiel {
     
@@ -21,16 +21,42 @@ class Spiel {
         Tempel erdeTempel = new Tempel("gruen");
         Tempel feuerTempel = new Tempel("rot");
 
-        // Fragen erzeugen oder aus einer Datei lesen
-        Quizfrage qf1_oop1_t1 = new Quizfrage(0, "Welcher dieser Datentypen ist nicht primitiv?", "String", "int", "boolean");
+        /* Tempel 1 */
 
-        // Fragen in Pool
-        for(int i=0; i<5;i++) {
-            if(niveau == 1)
-                wasserTempel.getFragenpoolOOP1().put(i, qf1_oop1_t1);
-            else
-                wasserTempel.getFragenpoolOOP2().put(i, qf1_oop1_t1);
+        // Quizfragen zu OOP2 vom Tempel 1 manuell erzeugen
+        int index_t1_oop2 = 0;
+        Quizfrage qf1_t1_oop2 = new Quizfrage("In einer Liste stehen miteinander verkettete…", "a) Nutzlasten", "b) Knoten", "c) abstrakte Datentypen", 'b');
+        Quizfrage qf2_t1_oop2 = new Quizfrage("Wie viele Elemente umfasst die Suche in einer Liste der Länge n?", "a) n", "b) n/2", "c) log(n)", 'c');
+        Quizfrage qf3_t1_oop2 = new Quizfrage("Welchen Sichtbarzeitbegriff muss die Klasse Knoten beinhalten?", "a) public", "b) protected", "c) private", 'c');
+        Quizfrage qf4_t1_oop2 = new Quizfrage("Wie kann eine Liste in beide Richtungen iteriert werden?", "a) Mit einem ListIterator", "b) Mit zwei Instanzen von Iterator", "c) Das ist nicht möglich", 'a');
+        Quizfrage qf5_t1_oop2 = new Quizfrage("Wird es über die einzelnen Nutzlasten einer Liste iteriert?", "a) Nein", "b) Ja", "", 'a');
+        Quizfrage qf6_t1_oop2 = new Quizfrage("Welcher Aufruf gibt die nächste Nutzlast zurück?", "a) hasNext()", "b) next()", "c) next.next()", 'b');
+        Quizfrage qf7_t1_oop2 = new Quizfrage("Wie viele Nachfolger kann ein Knoten in einem Binärbaum haben?", "a) Zwei", "b) Keine", "c) a und b sind richtig", 'c');
+        Quizfrage qf8_t1_oop2 = new Quizfrage("Besitzen die Knoten eine Referenz zum Elternelement?", "a) Ja", "b) Nein", "", 'b');
+
+        // Quizfragen zu OOP2 vom Tempel 1 in Fragenpool vom Tempel 1 speichern
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf1_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf1_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf2_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf3_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf4_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf5_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf6_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf7_t1_oop2);
+        wasserTempel.getFragenpoolOOP1().put(++index_t1_oop2,qf8_t1_oop2);
+
+
+        // Pruefen, dass die Daten richtig gespeichert wurden
+        /*
+        for(Map.Entry<Integer,Quizfrage> entry : wasserTempel.getFragenpool_oop1().entrySet()) {
+            System.out.print(entry.getKey() + ": ");
+            System.out.println(entry.getValue().getFrage());
+            for(String antwort : entry.getValue().getAntwortsmoeglichkeiten()) {
+                System.out.println("antwort: " + antwort);
+            }
+            System.out.println("richtig: " + entry.getValue().getRichtigeAntwort());
         }
+        */
 
 
 
