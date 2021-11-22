54d7a6e5d1f06f737c49c33209c859c0346854b8
sarahwolff561 <sarah.wolff@yahoo.com>
2021-11-22 18:53:59 +0100

    add comments

--- uno-projekt/kartenspiel/PlusVierWunschKarte.java
+++ uno-projekt/kartenspiel/PlusVierWunschKarte.java
@@ -1,6 +1,7 @@
 package kartenspiel;
 
-/**
+/** Der Spieler, der diese Karte legt, entscheidet welche Farbe als nächstes gelegt werden soll.
+ * Zudem muss der Gegenspieler vier Karten von dem Kartenstapel ziehen.
  *
  */
 public class PlusVierWunschKarte extends Karte{
