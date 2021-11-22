1ef966f6cd123a84a8629afd258b2debb68f2e15
sarahwolff561 <sarah.wolff@yahoo.com>
2021-11-22 19:28:53 +0100

    add comments

--- uno-projekt/kartenspiel/Karte.java
+++ uno-projekt/kartenspiel/Karte.java
@@ -23,15 +23,10 @@ public abstract class Karte {
         //...
     }
 
-<<<<<<< HEAD
-    /** Prüft, ob eine Karte korrekt abgelegt wurde. 
-     * Eine Karte wurde richtig abgelegt, wenn die Nummer und/oder Farbe der abgelegten Karte mit der Karte auf dem Kartenstapel übereinstimmt
+    /**
+     * Prüft, ob eine Karte korrekt abgelegt wurde. 
+     * Dies ist der Fall, wenn die Nummer und/oder Farbe der abgelegten Karte mit der Karte auf dem Kartenstapel übereinstimmt
      * oder wenn die abgelegte Karte die Farbe schwarz hat.
-=======
-    /** Prüft, ob eine Karte richtig abgelegt wurde, d.h. ob Nummer und/oder Farbe mit der Karte auf dem Kartenstapel
-     * übereinstimmt
-     * 
->>>>>>> 54d7a6e5d1f06f737c49c33209c859c0346854b8
      * @param andereKarte
      * @return
      */
