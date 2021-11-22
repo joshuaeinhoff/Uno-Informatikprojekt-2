110d0fd6fea9fc8c7d2eb34b8548e5b91bf3c424
Joshi <joshualeinhoff@gmail.com>
2021-11-22 22:34:50 +0100

    KI Schlau erstmal fertig..

--- uno-projekt/kartenspiel/KISchlau.java
+++ uno-projekt/kartenspiel/KISchlau.java
@@ -7,12 +7,29 @@ public class KISchlau extends Spieler{
     }
 
     public Karte karteSpielen(Karte aktuelleKarte, KartenStapel kartenStapel, boolean karteGezogen){
-        //ist 2+ Spielbar?ist AussetzenKarte spielbar?ist RetourKarte spielbar?
+        //ist AussetzenKarte spielbar?ist RetourKarte spielbar?
+        //ist 2+ Spielbar?
         //ist Farbkarte Spielbar?
-        //+4 Wunschkarte Spielbar?Wunschkarte Spielbar?
-        int karteAnzahl = hand.length;
-        if(karteGezogen != true){
-
+        //+4 Wunschkarte Spielbar?
+        //Wunschkarte Spielbar?
+        //sonst karte ziehen
+        if(existiertAussetzen(aktuelleKarte)){
+            return findAussetzenKarte(aktuelleKarte);
+        }else if(existiertRetourKarte(aktuelleKarte)){
+            return findRetourKarte(aktuelleKarte);
+        }else if(existiertPlusZwei(aktuelleKarte)){
+            return findPlusZwei(aktuelleKarte);
+        }else if(existiertFarbKarte(aktuelleKarte)){
+            return findFarbKarte(aktuelleKarte);
+        }else if(existiertPlusVierWunschKarte(aktuelleKarte)){
+            return findPlusVierWunschKarte(aktuelleKarte);
+        }else if(existiertWunschKarte(aktuelleKarte)){
+            return findWunschKarte(aktuelleKarte);
+        } else if(karteGezogen != true){
+            karteZiehen(kartenStapel);
+            karteSpielen(aktuelleKarte, kartenStapel, true);
+        }else{
+            return null;
         }
         return null;
     }
@@ -74,6 +91,49 @@ public class KISchlau extends Spieler{
     public Karte findPlusZwei(Karte aktuelleKarte){
         for(int i = 0; i < hand.length; i++){
             if(hand[i].istPlusZweiKarte()&&hand[i].istSpielbar(aktuelleKarte)){
+                return hand[i];
+            }
+        }
+        return null;
+    }
+    public Karte findAussetzenKarte(Karte aktuelleKarte){
+        for(int i = 0; i < hand.length; i++){
+            if(hand[i].istAussetzenKarte()&&hand[i].istSpielbar(aktuelleKarte)){
+                return hand[i];
+            }
+        }
+        return null;
+    }
+
+    public Karte findRetourKarte(Karte aktuelleKarte){
+        for(int i = 0; i < hand.length; i++){
+            if(hand[i].istRetourKarte()&&hand[i].istSpielbar(aktuelleKarte)){
+                return hand[i];
+            }
+        }
+        return null;
+    }
+
+    public Karte findFarbKarte(Karte aktuelleKarte){
+        for(int i = 0; i < hand.length; i++){
+            if(hand[i].istFarbKarte()&&hand[i].istSpielbar(aktuelleKarte)){
+                return hand[i];
+            }
+        }
+        return null;
+    }
+    public Karte findPlusVierWunschKarte(Karte aktuelleKarte){
+        for(int i = 0; i < hand.length; i++){
+            if(hand[i].istPlusVierWunschKarte()&&hand[i].istSpielbar(aktuelleKarte)){
+                return hand[i];
+            }
+        }
+        return null;
+    }
+
+    public Karte findWunschKarte(Karte aktuelleKarte){
+        for(int i = 0; i < hand.length; i++){
+            if(hand[i].istWunschKarte()&&hand[i].istSpielbar(aktuelleKarte)){
                 return hand[i];
             }
         }
