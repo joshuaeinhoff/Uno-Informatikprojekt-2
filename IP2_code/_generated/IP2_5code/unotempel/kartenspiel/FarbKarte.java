5304344bfb35a405607dde580e701ea949fd54ee
Teresa Rodriguez Fuentes <tsrodf@gmail.com>
2022-03-17 15:56:14 +0100

    deleted

--- IP2_code/_generated/IP2_5code/unotempel/kartenspiel/FarbKarte.java
+++
@@ -1,42 +0,0 @@
-package unotempel.kartenspiel;
-
-/**<br>
- *<br>
- <br>*/
-public class FarbKarte extends Karte{
-
-    /**<br>
-     * Konstruktor erzeugt einer Farbkarte mit den folgenden Eigenschaften<br>
-     * @param farbe - Farbe: blau, gelb, gr&uuml;n oder rot<br>
-     * @param nummer - Nummer: Zahl von 0 bis 9<br>
-     <br>*/
-    public FarbKarte(String farbe, int nummer){
-        super(farbe, nummer);
-    }
-
-
-    /**<br>
-     * Funktion gibt zur&uuml;ck, ob die Karte eine FarbKarte ist<br>
-     * @param kartenart - Art der Karte<br>
-     * @return true - falls die KartenArt passt, false - sonst<br>
-     <br>*/
-    public boolean istWelcheKarte(String kartenart){
-        return kartenart.equals("Farb");
-    }
-
-
-    /**<br>
-     * Funktion zum Durchf&uuml;hren der Aktion einer speziellen Karte<br>
-     * @return int - Code f&uuml;r die Aktion<br>
-     <br>*/
-    @Override
-    public int aktionDurchfuehren() {
-        // Code 0: Nichts machen
-        return 0;
-    }
-    
-    public String toString(){
-    	return "FarbKarte in der Farbe "+this.farbe+" und der Nummer "+this.nummer;
-    }
-
-} // Ende von FarbKarte
--- IP2_code/_generated/IP2_5code/unotempel/kartenspiel/WunschKarte.java
+++
@@ -1,51 +0,0 @@
-package unotempel.kartenspiel;
-
-/**<br>
- * Der Spieler, der diese Karte legt, entscheidet welche Farbe als n&auml;chstes gelegt werden soll. WunschKarten kann man immer legen, <br>
- * das hei&szlig;t man muss nicht auf die Farbe der vorherigen Karte achten. Wenn eine WunschKarte zu Beginn des Spiels gezogen wird,<br>
- * entscheidet der Spieler, der anfangen darf, welche Farbe als n&auml;chstes gelegt werden soll.<br>
- <br>*/
-public class WunschKarte extends Karte{
-
-    /**<br>
-     * Konstruktor mit default-Werten: Farbe schwarz und Nummer 13<br>
-     <br>*/
-    public WunschKarte(){
-        super("schwarz", 13);
-    }
-
-
-    /**<br>
-     * Konstruktor zum Erzeugen einer Wunschkarte mit einer bestimmten Farbe<br>
-     * @param neueFarbe - Neue Farbe (nur nachdem eine WunschKarte gespielt wird)<br>
-     <br>*/
-    public WunschKarte(String neueFarbe) {
-        super(neueFarbe, 13);
-    }
-
-
-    /**<br>
-     * Funktion gibt zur&uuml;ck, ob die Karte eine WunschKarte ist<br>
-     * @param kartenart - Art der Karte<br>
-     * @return true - falls die KartenArt passt, false - sonst<br>
-     <br>*/
-    public boolean istWelcheKarte(String kartenart){
-        return kartenart.equals("Wunsch");
-    }
-
-
-    /**<br>
-     * Funktion zum Durchf&uuml;hren der Aktion einer speziellen Karte<br>
-     * @return int - Code f&uuml;r die Aktion<br>
-     <br>*/
-    @Override
-    public int aktionDurchfuehren() {
-        // Code 3: Eine neue Farbe auswählen
-        return 3;
-    }
-    
-    public String toString(){
-    	return "WunschKarte";
-    }
-
-} // Ende von WunschKarte
